510a3d132c65326249fc45f144c33a20220cb2b0
Praisy Mittakadapa <praisymittakadapa@FT-MAC-PRAISYM.local>
2022-04-25 09:36:04 -0400

    added tell us about your business code

--- cypress/support/Page_Objects/SigninElements.js
+++ cypress/support/Page_Objects/SigninElements.js
@@ -1,117 +1,117 @@
-class SigninElements{
+class SigninElements {
 
-customerPage = {
-getpostalcode:() => cy.get('#postalCode'),
-getcustomername:() => cy.get('#customerName'),
-getinvoicenumber:() =>cy.get('#invoiceNumber'),
-getinvoiceDate:()=> cy.get('#invoiceDate'),
-getinvoiceamount:() =>cy.get('#invoiceTotal'),
-amountError:() => cy.get('.error'),
-getNetDays:() => cy.get('#netDays'),
-getbuttonuploadinvoice:()=>cy.get('.forward').contains('Finish'),
-getinvoicecreated:()=>cy.get('.notification-container'),
-getsuccessmessage:() =>  cy.get('.green-text.extra'),
-clickdropdown:() => cy.get('.active').click(),
-getupoadinvoice:() => cy.get('button.ui.primary.button.upload.circular'),
-getcardcotent:() => cy.get('.card-content-left'),
-getstartbusiness:() => cy.get('.ui.circular.button.bordered__small__dark-primary.ui.action'),
-uploadfiles:() => cy.get('.ui.primary.button.upload.circular'),
-getforwardbutton:() => cy.get('.ui.circular.button.forward'),
-getreversebutton:() => cy.get('.ui.button.reverse}'),
-geterror:() => cy.get('.error'),
-updatebutton:() => cy.get('ui.circular.primary.button.bold-text.u-mb+').invoke('show'),
-anyelement:() => cy.get('.ui.header.casing.header'),
-getinputfield:() => cy.get('.no-float > .clickable-text > .meta.black-text'),
-getbusinessname:() => cy.xpath('//*[@id="businessName"]'),
-getjobtitle:() => cy.xpath('//*[@id="jobTitle"]'),
-getfirstname:() => cy.xpath('//*[@id="firstName"]'),
-getlastname:() => cy.xpath('//*[@id="lastName"]'),
-getIframe:() => cy.get('#plaid-link-iframe-3'),
-getcontinuebutton:() => cy.xpath('//*[@id="aut-continue-button"]'),
-selectbank:() => cy.get('.ThreadsInstitutionResult__text').eq(0),
-getusername:() => cy.xpath('//*[@id="username"]'),
-getpassword:() => cy.xpath('//*[@id="password"]'),
-getcheckbox:() => cy.get('//*[@id="card-page-content"]/div[1]/div/div/form/div/div/input'),
-getinvoicelist:() => cy.get('.ui.fluid.container.u-mt.white-background'),
-getpaidbutton:() => cy.get('.get_paid_now-button'),
-getinvoicename:() => cy.get('.invoice__number'),
-getduedate:() => cy.get('.invoice__dueDate'),
-getheader:() => cy.get('.fund-header'),
-getblueinfotext:() => cy.get('.FTMessageBox_ftMessageBox__1QEbD'),
-getvelocitytext:() => cy.get('[data-test=here-is-how]'),
-gettooltip:() => cy.get('.column > .question'),
-getinvoicetotal:() => cy.get('.amount.bold-text.font-22'),
-getenteredinvoice:() => cy.get('.invoice__price'),
-getfundingtext:() => cy.get('[data-test=you-get] > .normal-text'),
-getpayorname:() => cy.get('[data-test=you-get] > .u-mt0'),
-getinvoicecustomer:() => cy.get('.invoice__customer__name'),
-getcustomerreviewtext:() => cy.get('.u-pt > :nth-child(1) > .u-pb'),
-getintroemailtext:() => cy.get('.ReviewConfirmation_list__3HuUR > :nth-child(1)'),
-getverificationtext:() => cy.get('.ReviewConfirmation_list__3HuUR > :nth-child(2)'),
-getnotetext:() => cy.get('.ReviewConfirmation_infoLine__1sOpn > b'),
-getcustomernote:() => cy.get('.u-mb > :nth-child(1) > b'),
-getcustomernote2:()=> cy.get(':nth-child(3) > b'),
-getcontactname:() => cy.get('.contact-list-toInputs > .sc-gzVnrw > .ui > input'),
-getcontactemail:() => cy.get('.contact-list-emailInputs > .sc-gzVnrw > .ui > input'),
-getmainbodytext:() => cy.get('.mainBodyText'),
-getreviewinvoicename:() => cy.get('.review-list-number'),
-getreviewduedate:() => cy.get('.review-list-date'),
-getreviewprice:() => cy.get('.review-list-price'),
-getemailaddress:() => cy.get('#email')
-}
-usercredential = {
-getusername:() => cy.get('#username'),
-getpassword:() => cy.get('#password'),
-getskipcontainer:() => cy.get('.skip-container > .ui'),
-fieldlevelerror:()=> cy.get('.error')    
-}
-velocitydashboard = {
-contentbutton :() => cy.get('.accordion__content_button'),
-getnavbar:()=> cy.get('.bars'),
-gettitle:() => cy.get('.title'),
-getyellowactioncard:() => cy.get('.ExpressAndVelocity_wrapper__JTgiJ'),
-getallctioncard:()=> cy.get('.action-card-carousel-spacing').find('.slick-slide'),
-getactioncardupload:() =>cy.get('.AddInvoiceButton_uploadButton__2Wner'),
-getinvoicecard:() => cy.get('.slick-current > :nth-child(1)')
+    customerPage = {
+        getpostalcode: () => cy.get('#postalCode'),
+        getcustomername: () => cy.get('#customerName'),
+        getinvoicenumber: () => cy.get('#invoiceNumber'),
+        getinvoiceDate: () => cy.get('#invoiceDate'),
+        getinvoiceamount: () => cy.get('#invoiceTotal'),
+        amountError: () => cy.get('.error'),
+        getNetDays: () => cy.get('#netDays'),
+        getbuttonuploadinvoice: () => cy.get('.forward').contains('Finish'),
+        getinvoicecreated: () => cy.get('.notification-container'),
+        getsuccessmessage: () => cy.get('.green-text.extra'),
+        clickdropdown: () => cy.get('.active').click(),
+        getupoadinvoice: () => cy.get('button.ui.primary.button.upload.circular'),
+        getcardcotent: () => cy.get('.card-content-left'),
+        getforwardbutton: () => cy.get('.ui.circular.button.forward'),
+        getreversebutton: () => cy.get('.ui.button.reverse}'),
+        geterror: () => cy.get('.error'),
+        
+    }
+    startbusiness = {
+        uploadfiles: () => cy.get('.ui.primary.button.upload.circular'),
+    }
+    fundingflow = {
+        gettablerow:() => cy.get('.table__body__row'),
+        getpaidbutton: () => cy.get('.table__body__row').find('.get_paid_now-button'),
+        getinvoicename: () => cy.get('.invoice__number'),
+        getduedate: () => cy.get('.invoice__dueDate'),
+        getheader: () => cy.get('.fund-header'),
+        getblueinfotext: () => cy.get('.FTMessageBox_ftMessageBox__1QEbD'),
+        getheading1:() => cy.get(':nth-child(3) > div > .normal-text'),
+        getheading2: () => cy.get('[data-test=credit-title]'),
+        getheading3:() => cy.get(':nth-child(5) > div > .normal-text'),
+        getheading4:() => cy.get(':nth-child(6) > div > .normal-text'),
+        getheading5:() => cy.get(':nth-child(7) > :nth-child(2) > .normal-text'),
+        getvelocitytext: () => cy.get('[data-test=here-is-how]'),
+        gettooltip: () => cy.get('.column > .question'),
+        gettooltiptext:() => cy.get('.top'),
+        getinvoicetotal: () => cy.get('.amount.bold-text.font-22'),
+        getenteredinvoice: () => cy.get('.invoice__price'),
+        getfundingtext: () => cy.get('[data-test=you-get] > .normal-text'),
+        getpayorname: () => cy.get('[data-test=you-get] > .u-mt0'),
+        getinvoicecustomer: () => cy.get('.invoice__customer__name'),
+        getcustomerreviewtext: () => cy.get('.u-pt > :nth-child(1) > .u-pb'),
+        getintroemailtext: () => cy.get('.ReviewConfirmation_list__3HuUR > :nth-child(1)'),
+        getverificationtext: () => cy.get('.ReviewConfirmation_list__3HuUR > :nth-child(2)'),
+        getnotetext: () => cy.get('.ReviewConfirmation_infoLine__1sOpn > b'),
+        getcustomernote: () => cy.get('.u-mb > :nth-child(1) > b'),
+        getcustomernote2: () => cy.get(':nth-child(3) > b'),
+        getcontactname: () => cy.get('.contact-list-toInputs > .sc-gzVnrw > .ui > input'),
+        getcontactemail: () => cy.get('.contact-list-emailInputs > .sc-gzVnrw > .ui > input'),
+        getmainbodytext: () => cy.get('.mainBodyText'),
+        getreviewinvoicename: () => cy.get('.review-list-number'),
+        getreviewduedate: () => cy.get('.review-list-date'),
+        getreviewprice: () => cy.get('.review-list-price'),
+        getemailaddress: () => cy.get('#email'),
+        getcontainer:() => cy.get('.ui.fluid.container'),
+        getreviewingstatus: () => cy.get('.invoice-table_body').find('.invoice__state')
+    }
 
-}
-elementsui ={
-getsignuphyperlink:()=> cy.get('.signup_prompt_parent').find('p'),
-forgotpasswordlink:() => cy.get('.forgot-password'),
-cardcontent:() => cy.get('#card-page-content')
-}
+    usercredential = {
+        getusername: () => cy.get('#username'),
+        getpassword: () => cy.get('#password'),
+        getskipcontainer: () => cy.get('.skip-container > .ui'),
+        fieldlevelerror: () => cy.get('.error')
+    }
+    velocitydashboard = {
+        contentbutton: () => cy.get('.accordion__content_button'),
+        getnavbar: () => cy.get('.bars'),
+        gettitle: () => cy.get('.title'),
+        getyellowactioncard: () => cy.get('.ExpressAndVelocity_wrapper__JTgiJ'),
+        getallctioncard: () => cy.get('.action-card-carousel-spacing').find('.slick-slide'),
+        getactioncardupload: () => cy.get('.AddInvoiceButton_uploadButton__2Wner'),
+        getinvoicecard: () => cy.get('.slick-current > :nth-child(1)')
 
-netDaysCount(){
-var netdayscount = [30,60,90];
-var randomnetDays = Math.floor(Math.random() * netdayscount.length);
-this.customerPage.getNetDays().type(netdayscount[randomnetDays])
-}
-getdatetoinvoicepage(){
-var today = new Date();
-var dd = String(today.getDate()-1)
-var mm = String(today.getMonth() + 1)
-var yyyy = today.getFullYear();
-today = (yyyy + '/' + mm + '/' + dd);
-this.customerPage.getinvoiceDate().type(today).type('{enter}')
-}
-enterrandomnumber(){
-let x = Math.floor((Math.random() * 10))
-this.customerPage.getinvoiceamount().type(x)
-if(x == 0){
-cy.get('.error').contains('Invalid Invoice Total');
-}
-}
+    }
+    elementsui = {
+        getsignuphyperlink: () => cy.get('.signup_prompt_parent').find('p'),
+        forgotpasswordlink: () => cy.get('.forgot-password'),
+        cardcontent: () => cy.get('#card-page-content')
+    }
 
-entercustomern(value){
-this.customerPage.getcustomername().type(value)
-}
+    netDaysCount() {
+        var netdayscount = [30, 60, 90];
+        var randomnetDays = Math.floor(Math.random() * netdayscount.length);
+        this.customerPage.getNetDays().type(netdayscount[randomnetDays])
+    }
+    getdatetoinvoicepage() {
+        var today = new Date();
+        var dd = String(today.getDate() - 1)
+        var mm = String(today.getMonth() + 1)
+        var yyyy = today.getFullYear();
+        today = (yyyy + '/' + mm + '/' + dd);
+        this.customerPage.getinvoiceDate().type(today).type('{enter}')
+    }
+    enterrandomnumber() {
+        let x = Math.floor((Math.random() * 10))
+        this.customerPage.getinvoiceamount().type(x)
+        if (x == 0) {
+            cy.get('.error').contains('Invalid Invoice Total');
+        }
+    }
 
-enterpostalcode(value){
-this.elements.getpostalcode().type(value)
-}
-// getnavbar()
-// {
-// return cy.get('.u-mt')
-// }
+    entercustomern(value) {
+        this.customerPage.getcustomername().type(value)
+    }
+
+    enterpostalcode(value) {
+        this.elements.getpostalcode().type(value)
+    }
+    // getnavbar()
+    // {
+    // return cy.get('.u-mt')
+    // }
 }
 export default SigninElements;
