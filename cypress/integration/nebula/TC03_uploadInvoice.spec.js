0bafc2dcac72bf9b3bc391d7101459e7b623cfbc
ManpreetChopraKaur <62396219+ManpreetChopraKaur@users.noreply.github.com>
2022-06-28 23:54:36 -0400

    Update TC03_uploadInvoice.spec.js

--- cypress/integration/nebula/TC03_uploadInvoice.spec.js
+++ cypress/integration/nebula/TC03_uploadInvoice.spec.js
@@ -1,6 +1,6 @@
-import {invoiceSelectors, InvoiceUpload} from "../../support/Page_Objects/invoiceElements";
+import { checkCard, invoiceSelectors, InvoiceUpload } from "../../support/Page_Objects/invoiceElements";
 import {
-  verifyNavigation
+  verifyNavigation, visit,
 } from "../../support/Helpers/common/navigation";
 import {
   clearInputValue,
@@ -22,7 +22,7 @@ import {
   messageSelectors,
   messageTexts,
 } from "../../support/Helpers/common/messages";
-import {checkTooltip, tooltipSelectors, tooltipTexts} from "../../support/Helpers/common/tooltip";
+import { checkTooltip, tooltipTexts } from "../../support/Helpers/common/tooltip";
 import {randomChars, randomLetter, randomNum} from "../../support/Helpers/common";
 
 
@@ -69,8 +69,8 @@ describe("Upload your first invoice", () => {
           .verifyFormHeaders()
 
         verifyInputLabels(invoiceLabels)
-        checkTooltip(tooltipSelectors.inputLabel, 'Invoice Number', tooltipTexts.invoiceNumber)
-        checkTooltip(tooltipSelectors.inputLabel, 'Invoice Date', tooltipTexts.invoiceDate)
+        checkTooltip('Invoice Number', tooltipTexts.invoiceNumber)
+        checkTooltip('Invoice Date', tooltipTexts.invoiceDate)
 
         //validate error of customer name field
         fillInputWithValue(invoiceSelectors.customer, randomLetter(8))
@@ -93,28 +93,23 @@ describe("Upload your first invoice", () => {
         //enter invoice number
         fillInputWithValue(invoiceSelectors.number, randomChars(6))
         //validate error of date field
+        fillInputWithValue(invoiceSelectors.date,"2022-01-01")
+        checkMessage(messageSelectors.error, messageTexts.dateError)
+        clearInputValue(invoiceSelectors.date)
         
-        //checkMessage(messageSelectors.error, messageTexts.dateError)
-        //clearInputValue(invoiceSelectors.date)
-//         cy.get('#invoiceDate').click()
-//         fillInputWithValue(invoiceSelectors.date,"2022-01-01")
-        //enter invoice datet
-//      cy.get('#dueDate').click();
-            
-//              cy.get('#invoiceDate').click().type("2022-05-01")
-        fillInputWithValue(invoiceSelectors.date,"2022-05-01")
-        //enter invoice datet
-//      cy.get('#dueDate').click().type("2022-07-01")
+        //enter invoice date
+        invoiceUpload
+          .pickDate()
         
         //validate error of due date field
         fillInputWithValue(invoiceSelectors.due, "2022-01-01")
         clearInputValue(invoiceSelectors.due)
         checkMessage(messageSelectors.error, messageTexts.invalidDueDate)
-      
+
         //enter due date
-        invoiceUpload.pickDueDate()
-         
-          //verifyPaymentDays()
+        invoiceUpload 
+          .pickDueDate()
+          .verifyPaymentDays()
         
         //check error of invoice total
         fillInputWithValue(invoiceSelectors.total, randomNum(3))
